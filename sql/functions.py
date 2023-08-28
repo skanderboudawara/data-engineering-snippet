174352333d2a83235fc0c56cc95c92238dbaab67
Skander Boudawara <108568261+skanderboudawara@users.noreply.github.com>
2023-08-28 15:45:25 +0200

    add extract_array_occurence

--- sql/functions.py
+++ sql/functions.py
@@ -85,3 +85,62 @@ def unpivot_table(
     )
 
     return self
+
+
+def extract_array_occurence(
+    column_array: Union[str, Column],
+    tolerance_user : float = 0.0,
+    most_frequent : bool = True,
+) -> Column:
+    """
+    This function will look for a cluster with most (or least) occurences with user tolerance
+    eg:
+        For an array [1, 1, 1, 2, 3] tolerance_user 0 and most_frequent True
+        this function will return [1, 1, 1]
+
+    eg2:
+        For an array [1, 1, 1, 2, 2, 2, 3] tolerance_user 0 and most_frequent True
+        this function will return [1, 1, 1, 2, 2, 2]
+
+    :param column_array: (str or Column), column name that contains the array information.
+
+    :param tolerance_user: (float), a percentage of tolerance between values. Must be between [0.0-1.0]. Default to 0.0
+
+    :param most_frequent: (bool), True to extract most frequent, False to extract the least frequent. Default True
+
+    :returns: (Column), array containing the occurrence
+    """
+
+    if not isinstance(column_array, (str, Column)):
+        raise TypeError("column_array must be a string or Column")
+
+    if not isinstance(tolerance_user, float):
+        raise TypeError("tolerance_user must be a float")
+
+    if not (0.0 <= tolerance_user <= 1.0):
+        raise ValueError("argument tolerance_user must be between 0 & 1 in float")
+
+    if not isinstance(most_frequent, bool):
+        raise TypeError("most_frequent must be a boolean")
+
+    column_array = column_array if isinstance(column_array, Column) else col(column_array)
+    column_array = sort_array(column_array, False)
+
+    compute_occurences = transform_(
+        column_array,
+        lambda y : aggregate_(
+            column_array,
+            lit(0.0),
+            lambda acc, x: when(x.between(y*(1-tolerance_user), y*(1+tolerance_user)), acc+lit(1.0)).otherwise(acc)
+        )
+    )
+
+    max_or_min_occurence = array_max(compute_occurences) if most_frequent else array_min(compute_occurences)
+
+    return filter_(
+        arrays_zip(
+            column_array.alias("tmp_array_of_working_columns"),
+            compute_occurences.alias("compute_occurences")
+        ),
+        lambda x: x["compute_occurences"] == max_or_min_occurence
+    ).tmp_array_of_working_columns
