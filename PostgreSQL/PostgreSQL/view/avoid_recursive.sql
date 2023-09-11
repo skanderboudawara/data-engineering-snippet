3a87de93997952bbfb26ac1c729b504e2a213613
Skander Boudawara <108568261+skanderboudawara@users.noreply.github.com>
2023-09-11 21:04:19 +0200

    Create avoid_recursive.sql

---
+++ PostgreSQL/PostgreSQL/view/avoid_recursive.sql
@@ -0,0 +1,47 @@
+DROP TABLE IF EXISTS your_table;
+CREATE TABLE your_table (
+    father character varying,
+    child character varying
+);
+
+-- Insert data into the table
+INSERT INTO your_table (father, child)
+VALUES
+   ('A', 'B'),
+   ('B', 'C'),
+   ('C', 'D'),
+   ('D', 'E'),
+   ('E', 'C'),
+   ('E', 'F');
+
+-- to avoid infinite loops
+WITH RECURSIVE all_links(
+    father,
+    child,
+    depth,
+    all_fathers
+) AS (
+    SELECT
+        *,
+        0 as depth,
+        array[father] as all_fathers
+    FROM 
+        your_table
+    WHERE
+        father = 'C'
+    UNION
+    ALL
+    SELECT
+        YourTable.*,
+        AllLinks.depth + 1 as depth,
+        AllLinks.all_fathers || YourTable.child as all_fathers
+    FROM
+        all_links as AllLinks,
+        your_table as YourTable
+    WHERE
+        YourTable.father = AllLinks.child 
+		AND
+        NOT YourTable.child = ANY(AllLinks.all_fathers)
+)
+SELECT *
+FROM all_links
